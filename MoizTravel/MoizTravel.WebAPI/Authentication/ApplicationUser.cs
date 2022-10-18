ff6135cc66a1342b07df3153c4f7ad8c20842b16
nguyenhiep <nguyenhiepskt1@gmail.com>
2022-10-18 16:26:41 +0700

    Done
    Signed-off-by: nguyenhiep <nguyenhiepskt1@gmail.com>

---
+++ MoizTravel/MoizTravel.WebAPI/Authentication/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MoizTravel.WebAPI.Authentication
+{
+    public class ApplicationUser: IdentityUser
+    {
+    }
+}
---
+++ MoizTravel/MoizTravel.WebAPI/DbContext/MoizTravelDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MoizTravel.WebAPI.Authentication;
+
+namespace MoizTravel.WebAPI.DbContext
+{
+    public class MoizTravelDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public MoizTravelDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
